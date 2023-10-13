abec75919f6bb9b54f4a4edc890ece5c6f349130
Ruslan <ruslan_savkin_nn@mail.ru>
2023-10-13 14:18:03 +0300

    исправил ошибки

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int final_score = 93;
+        int finalScore = 93;
         int replenishment = 1500;
         int bonus = 0;
-        if (replenishment > 1000){
-            bonus = replenishment/100 * 1;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100 * 1;
         }
-        System.out.print("Итоговый счет = "+(final_score+replenishment) + "руб.");
-        System.out.print("\nНачисленные бонусы = "+ bonus + "руб.");
+        System.out.print("Итоговый счет = " + (finalScore + replenishment) + "руб.");
+        System.out.print("\nНачисленные бонусы = " + bonus + "руб.");
     }
 }
