1f512e0e4c5e17cc581c96f713b7880c8926081e
Ruslan <ruslan_savkin_nn@mail.ru>
2023-10-14 08:52:39 +0300

    Поправлен вывод итогового счёт

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int finalScore = 93;
+        int finalScore = 100;
         int replenishment = 1500;
         int bonus = 0;
         if (replenishment > 1000) {
             bonus = replenishment / 100 * 1;
         }
-        System.out.print("Итоговый счет = " + (finalScore + replenishment) + "руб.");
+        System.out.print("Итоговый счет = " + (finalScore + replenishment + bonus) + "руб.");
         System.out.print("\nНачисленные бонусы = " + bonus + "руб.");
     }
 }
